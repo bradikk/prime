d68f1d39dc883ad0cdb3257257636e3f8e184581
bradikk <124538028+bradikk@users.noreply.github.com>
2023-02-23 13:29:30 -0800

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,52 @@
+#include <cs50.h>
+#include <stdio.h>
+
+bool prime(int number);
+
+int main(void)
+{
+    int min;
+    do
+    {
+        min = get_int("Minimum: ");
+    }
+    while (min < 1);
+
+    int max;
+    do
+    {
+        max = get_int("Maximum: ");
+    }
+    while (min >= max);
+
+    for (int i = min; i <= max; i++)
+    {
+        if (prime(i))
+        {
+            printf("%i\n", i);
+        }
+    }
+}
+
+bool prime(int number)
+{
+    // cannot equal one
+    if (number <= 1)
+    {
+        return false;
+    }
+    // two or three true bc prime
+    if (number == 2 || number == 3)
+    {
+        return true;
+    }
+    //start at 2, cannot divide number by itself
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
